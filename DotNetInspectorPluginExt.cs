a6cc4dbfe6c3838ee9277541aa6cf959b8b0da6d
KN4CK3R <admin@oldschoolhack.me>
2017-01-19 21:13:41 +0100

    Initial commit of base project files.

---
+++ DotNetInspectorPluginExt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using ReClassNET;
+using ReClassNET.Plugins;
+
+namespace DotNetInspectorPlugin
+{
+	public class DotNetInspectorPluginExt : Plugin
+	{
+		private IPluginHost host;
+
+		public override Image Icon => Properties.Resources.logo;
+
+		public override bool Initialize(IPluginHost pluginHost)
+		{
+			//System.Diagnostics.Debugger.Launch();
+
+			if (host != null)
+			{
+				Terminate();
+			}
+
+			if (pluginHost == null)
+			{
+				throw new ArgumentNullException(nameof(pluginHost));
+			}
+
+			host = pluginHost;
+
+			return true;
+		}
+
+		public override void Terminate()
+		{
+			host = null;
+		}
+	}
+}
