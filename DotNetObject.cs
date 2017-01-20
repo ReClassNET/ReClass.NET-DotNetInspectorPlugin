3c3da1805f4ce9c5136c2551ac7ace1b05dc1e11
KN4CK3R <admin@oldschoolhack.me>
2017-01-20 14:37:06 +0100

    Added DotNetObjectCollector.

---
+++ DotNetObject.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using Microsoft.Diagnostics.Runtime;
+
+namespace DotNetInspectorPlugin
+{
+	[DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}")]
+	class DotNetObject : IComparable<DotNetObject>
+	{
+		public string DebuggerDisplay => $"{Type.Name} {Name}";
+
+		public DotNetObject Parent { get; }
+
+		public ulong Reference { get; }
+		public string Name { get; }
+		public ClrType Type { get; }
+		public ClrInstanceField Field { get; }
+
+		public bool IsValueType =>
+			IsFloatingPointValueType ||
+			IsIntegerValueType ||
+			Type.ElementType == ClrElementType.Boolean ||
+			Type.ElementType == ClrElementType.Char ||
+			Type.ElementType == ClrElementType.FunctionPointer ||
+			Type.ElementType == ClrElementType.String;
+
+		public bool IsFloatingPointValueType =>
+			Type.ElementType == ClrElementType.Double ||
+			Type.ElementType == ClrElementType.Float;
+
+		public bool IsIntegerValueType =>
+			Type.ElementType == ClrElementType.Int8 ||
+			Type.ElementType == ClrElementType.Int16 ||
+			Type.ElementType == ClrElementType.Int32 ||
+			Type.ElementType == ClrElementType.Int64 ||
+			Type.ElementType == ClrElementType.UInt8 ||
+			Type.ElementType == ClrElementType.UInt16 ||
+			Type.ElementType == ClrElementType.UInt32 ||
+			Type.ElementType == ClrElementType.UInt64 ||
+			Type.ElementType == ClrElementType.NativeInt ||
+			Type.ElementType == ClrElementType.NativeUInt;
+
+		public List<DotNetObject> Children { get; } = new List<DotNetObject>();
+
+		public DotNetObject(ulong reference, ClrType type, string name)
+			: this(null, null, reference, type, name)
+		{
+
+		}
+
+		public DotNetObject(DotNetObject parent, ClrInstanceField field, ulong reference, ClrType type, string name)
+		{
+			Contract.Requires(type != null);
+
+			Parent = parent;
+			Field = field;
+			Reference = reference;
+			Type = type;
+
+			var rootName = GetRootName();
+			if (!string.IsNullOrEmpty(rootName) && name.StartsWith(rootName))
+			{
+				name = name.Remove(0, rootName.Length);
+			}
+
+			Name = name;
+		}
+
+		public string GetFullName()
+		{
+			return GetFullNamespace();
+		}
+
+		private string GetRootName()
+		{
+			if (Parent == null)
+			{
+				return Name == null ? string.Empty : Name;
+			}
+
+			return Parent.GetRootName();
+		}
+
+		private string GetFullNamespace()
+		{
+			if (Parent == null)
+			{
+				return GetRootName();
+			}
+
+			return Parent.GetFullNamespace() + "." + Name;
+		}
+
+		public object GetValue()
+		{
+			return Field.GetValue(Reference);
+		}
+
+		public string GetFormattedValue(bool hex)
+		{
+			const string NullString = "null";
+
+			if (Field != null && IsValueType)
+			{
+				var value = GetValue();
+
+				if (Type.ElementType == ClrElementType.Boolean)
+				{
+					return (bool)value ? bool.TrueString : bool.FalseString;
+				}
+				else if (Type.ElementType == ClrElementType.Char)
+				{
+					return $"'{value}'";
+				}
+				else if (Type.ElementType == ClrElementType.String)
+				{
+					if (value == null)
+					{
+						return NullString;
+					}
+					return $"\"{value}\"";
+				}
+				else if (IsFloatingPointValueType || IsIntegerValueType)
+				{
+					var formattable = value as IFormattable;
+					if (formattable != null)
+					{
+						string format = null;
+						if (IsIntegerValueType && hex)
+						{
+							format = "X";
+						}
+						return formattable.ToString(format, CultureInfo.InvariantCulture);
+					}
+					return value.ToString();
+				}
+				else if (Type.ElementType == ClrElementType.FunctionPointer)
+				{
+					return $"0x{value:X}";
+				}
+			}
+
+			return $"{{{Type}}}";
+		}
+
+		public void SetValue(object value)
+		{
+
+		}
+
+		public int CompareTo(DotNetObject other)
+		{
+			return Name.CompareTo(other?.Name);
+		}
+	}
+}
---
+++ DotNetObjectCollector.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Microsoft.Diagnostics.Runtime;
+
+namespace DotNetInspectorPlugin
+{
+	class DotNetObjectCollector
+	{
+		private ClrHeap heap;
+
+		public DotNetObjectCollector(ClrHeap heap)
+		{
+			Contract.Requires(heap != null);
+
+			this.heap = heap;
+		}
+
+		public IEnumerable<DotNetObject> EnumerateObjects()
+		{
+			var roots = new List<DotNetObject>();
+			var visited = new HashSet<ulong>();
+
+			foreach (var root in heap.EnumerateRoots())
+			{
+				heap.EnumerateObjectAddresses();
+				if (root == null || root.Type == null || root.Name == null)
+				{
+					continue;
+				}
+
+				var name = root.Name;
+				string prefix;
+				if ((prefix = ClrGlobals.ExcludePrefix.FirstOrDefault(p => name.StartsWith(p, StringComparison.InvariantCultureIgnoreCase))) != null)
+				{
+					name = name.Substring(prefix.Length);
+				}
+				if (ClrGlobals.ExcludeRootNamespaces.Any(n => name.StartsWith(n, StringComparison.InvariantCultureIgnoreCase)))
+				{
+					continue;
+				}
+
+				if (ClrGlobals.ExcludeNames.Any(n => name.StartsWith(n, StringComparison.InvariantCultureIgnoreCase)))
+				{
+					continue;
+				}
+
+				var dot = name.LastIndexOf('.');
+				if (dot != -1)
+				{
+					name = name.Substring(dot + 1);
+				}
+
+				if (!visited.Add(root.Object))
+				{
+					continue;
+				}
+
+				var obj = new DotNetObject(root.Object, root.Type, name);
+				roots.Add(obj);
+
+				RecursiveEnumerateObjects(visited, obj, obj.Reference, obj.Type);
+			}
+
+			return roots;
+		}
+
+		private void RecursiveEnumerateObjects(HashSet<ulong> visited, DotNetObject parent, ulong reference, ClrType clrType)
+		{
+			Contract.Requires(visited != null);
+			Contract.Requires(parent != null);
+
+			foreach (var field in clrType.Fields)
+			{
+				if (ClrGlobals.ExcludeNames.Any(n => field.Name.StartsWith(n, StringComparison.InvariantCultureIgnoreCase)))
+				{
+					continue;
+				}
+
+				var fieldObj = new DotNetObject(parent, field, reference + (ulong)field.Offset, field.Type, field.Name);
+
+				parent.Children.Add(fieldObj);
+
+				if (field.ElementType == ClrElementType.Object || field.ElementType == ClrElementType.Array || field.ElementType == ClrElementType.Struct)
+				{
+					try
+					{
+						var refObjectAddress = (ulong)field.GetValue(reference);
+						if (refObjectAddress == 0)
+						{
+							continue;
+						}
+
+						if (!visited.Add(refObjectAddress))
+						{
+							continue;
+						}
+
+						var refObjectClrType = heap.GetObjectType(refObjectAddress);
+						if (refObjectClrType == null)
+						{
+							continue;
+						}
+
+						RecursiveEnumerateObjects(visited, fieldObj, refObjectAddress, refObjectClrType);
+					}
+					catch (Exception ex)
+					{
+						// field.GetValue may throw
+					}
+				}
+			}
+
+			parent.Children.Sort();
+		}
+	}
+
+	public class ClrGlobals
+	{
+		public static readonly string[] ExcludeRootNamespaces = new string[]
+		{
+			"System.",
+			"Microsoft.",
+			"MS.",
+			"<CppImplementationDetails>.",
+			"<CrtImplementationDetails>.",
+			"Newtonsoft.",
+		};
+
+		public static readonly string[] ExcludeNames = new string[]
+		{
+			"finalization handle",
+			"strong handle",
+			"pinned handle",
+			"RefCount handle",
+			"local var"
+		};
+
+		public static readonly string[] ExcludePrefix = new string[]
+		{
+			"static var "
+		};
+	}
+}
